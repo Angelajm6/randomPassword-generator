4c73f725dd17d125bb8c9a23724ee4157c430f69
angelajm6 <angelajaume.m@gmail.com>
2022-11-20 19:41:24 -0500

    defined a function and added a return

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  console.log("You clicked the button!");
+// Prompt the user for the password criteria
+//  1. Password lenght <128
+//  2. lowercase, uppercase, numbers, special characters
+// Validate the input
+// Generate password based on the criteria
+
+
+//Display password on the page
+return "Your password will go here!";
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
