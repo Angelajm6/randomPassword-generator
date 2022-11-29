f849628115ebc3828ed2dd2ca31b6d9bfc30d977
angelajm6 <angelajaume.m@gmail.com>
2022-11-28 23:06:42 -0500

    Included document.querySelector

--- Develop/script.js
+++ Develop/script.js
@@ -5,22 +5,24 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword(){
   console.log("You clicked the button!");
 
-var specialCharArr =["!","?","ª","@", "#","$","%","/","^", "&", "*", "+","-","(", ")",",","<",">"];
+//Generate password based on these criteria
 var lowerCaseArr =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseArr =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numberArr =["0","1","2","3","4","5","6","7","8","9"]; 
-//  1. Password lenght <128
-//  2. lowercase, uppercase, numbers, special characters
-// Validate the input
-// Generate password based on the criteria
+var specialCharArr =["!","?","ª","@", "#","$","%","/","^", "&", "*", "+","-","(", ")",",","<",">"];
+
+//Validate input
 return "Your password will go here!";
 }
-
 
 //Event listener to create button
 generateBtn.addEventListener("click",writePassword);
 
+
 function writePassword() {
+  var correctPrompts=getPrompts();
+  var passwordText=document.querySelector("#password");
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
