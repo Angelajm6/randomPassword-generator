99b4f29d9aa41965ade093bd343e6b9e8396d7b2
angelajm6 <angelajaume.m@gmail.com>
2022-11-28 22:51:01 -0500

    Added number Array

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,7 @@ function generatePassword(){
 var specialCharArr =["!","?","ª","@", "#","$","%","/","^", "&", "*", "+","-","(", ")",",","<",">"];
 var lowerCaseArr =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseArr =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-// Prompt the user for the password criteria
+var numberArr =["0","1","2","3","4","5","6","7","8","9"]; 
 //  1. Password lenght <128
 //  2. lowercase, uppercase, numbers, special characters
 // Validate the input
