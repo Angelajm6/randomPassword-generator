a12ebe33d4a74d42d4ce6ae45b9198ed864238d3
angelajm6 <angelajaume.m@gmail.com>
2022-11-29 18:35:14 -0500

    corrected a spelling error

--- script.js
+++ script.js
@@ -2,17 +2,16 @@ var characterLenght=8; // lenght of 8 characters by default
 var choiceArr = [];
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
-  console.log("You clicked the button!");
-
 //Generate password based on these criteria
 var lowerCaseArr =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseArr =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numberArr =["0","1","2","3","4","5","6","7","8","9"]; 
 var specialCharArr =["!","?","ª","@", "#","$","%","/","^", "&", "*", "+","-","(", ")",",","<",">",];
 
-//Validate input
-return "Your password will go here!";
+
+function generatePassword(){
+  console.log("You clicked the button!");
+return "Your password will go here!"; //Validate input
 }
 
 //Event listener to create button
@@ -36,7 +35,7 @@ function writePassword() {
 function generatePassword(){
 var password = "";
 for (var i=0; i<characterLenght; i++){
-    var randomIndex = Math.floor(Math.random() * choiceArr.lenght);
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
     password = password + choiceArr[randomIndex];
 }
 return password;
