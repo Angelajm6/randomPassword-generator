e83c9e19e89e233816b16e599e84fd18c3a6194b
angelajm6 <angelajaume.m@gmail.com>
2022-11-28 22:59:24 -0500

    Added an EventListener to generate button

--- Develop/script.js
+++ Develop/script.js
@@ -13,14 +13,13 @@ var numberArr =["0","1","2","3","4","5","6","7","8","9"];
 //  2. lowercase, uppercase, numbers, special characters
 // Validate the input
 // Generate password based on the criteria
-
-
-
 return "Your password will go here!";
 }
 
 
-// Write password to the #password input
+//Event listener to create button
+generateBtn.addEventListener("click",writePassword);
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
