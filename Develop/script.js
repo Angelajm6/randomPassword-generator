17e4efa8d7252a89d50818a78c2f2df3c3651164
angelajm6 <angelajaume.m@gmail.com>
2022-11-28 22:47:30 -0500

    added lower case Array

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,7 @@ function generatePassword(){
   console.log("You clicked the button!");
 
 var specialCharArr =["!","?","ª","@", "#","$","%","/","^", "&", "*", "+","-","(", ")",",","<",">"];
+var lowerCaseArr =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 // Prompt the user for the password criteria
 //  1. Password lenght <128
 //  2. lowercase, uppercase, numbers, special characters
